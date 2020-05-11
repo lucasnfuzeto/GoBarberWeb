868179f59f1a8d867ef6eb5a77167021aff47cc7
kukajp3 <kukajp@gmail.com>
2020-05-11 19:05:28 -0300

    gobarber web

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+
+import './config/ReactotronConfig';
+
+import history from './services/history';
+import Routes from './routes';
+
+import store from './store';
+
+import GlobalStyle from './styles/global';
+
+export default function App() {
+  return (
+    <Provider store={store}>
+      <Router history={history}>
+        <Routes />
+        <GlobalStyle />
+      </Router>
+    </Provider>
+  );
+}
